0db62302535862085349608e16f9aa152e099237
Shrutika-Shinde <sanjayshinde891974@gmail.com>
2025-04-20 07:40:38 +0530

    Add files via upload

---
+++ google maps/index.js
@@ -0,0 +1,11 @@
+/**
+ * @license
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+async function initMap() {
+  console.log("Maps JavaScript API loaded.");
+}
+
+window.initMap = initMap;
